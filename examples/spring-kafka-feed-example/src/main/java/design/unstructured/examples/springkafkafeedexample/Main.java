3789948eab88e29e2d7ae7bc6e3836c1a69ac4b1
Chris Carver <ccarver01@gmail.com>
2020-01-13 13:57:55 -0500

    linking examples repository

--- examples/spring-kafka-feed-example/src/main/java/design/unstructured/examples/springkafkafeedexample/Main.java
+++ examples/spring-kafka-feed-example/src/main/java/design/unstructured/examples/springkafkafeedexample/Main.java
@@ -1,6 +1,7 @@
 package design.unstructured.examples.springkafkafeedexample;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.SpringApplication;
@@ -15,21 +16,17 @@ public class Main {
 	}
 
 	@Bean
-	public Function<KStream<String, String>, KStream<?, WordCount>> process() {
-
-		return input -> input
-                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                .map((key, value) -> new KeyValue<>(value, value))
-                .groupByKey(Serialized.with(Serdes.String(), Serdes.String()))
-                .windowedBy(TimeWindows.of(5000))
-                .count(Materialized.as("word-counts-state-store"))
-                .toStream()
-                .map((key, value) -> new KeyValue<>(key.key(), new WordCount(key.key(), value,
-						new Date(key.window().start()), new Date(key.window().end()))));
-						
+	private Consumer<KStream<String, String>> consume() {
 		return input -> input.foreach((key, value) -> {
-
+			System.out.println("Key: " + key + " Value: " + value);
 		});
+	}
+
+	@Bean
+	public Function<KStream<String, String>, KStream<String, String>> consumeAndProduce() {
+
+		return null; // input -> input.foreach((key,value) -> {});
+
 	}
 
 }
